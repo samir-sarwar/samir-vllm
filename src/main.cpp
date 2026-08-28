2762801851738bfa3888fb69d50be35e8ac63f6a
samir-sarwar <samirsarwaremail@gmail.com>
2026-08-28 18:12:17 +0000

    load safetensor model file and read headersize

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <queue>
 #define JSON_USE_IMPLICIT_CONVERSIONS 0
 #include <nlohmann/json.hpp> // grabs json header for functions to parse and create json objects in cpp
-
+#include <fstream>
 #define B_TO_GB 1024*1024*1024
 
 using json = nlohmann::json; // local alias 
@@ -33,9 +33,35 @@ int checkGPUStatus(){
     std::cout << "Free memory: " << (free_mem / (1024*1024*1024)) << "GB, total memory: " << total_mem / (1024*1024*1024) << "GB\n";
     return 0;
 }
+
+void loadModelHeader(){
+    std::string path = "models/llama-3.2-1b-instruct/model.safetensors"; // set local path
+    std::ifstream safetensors_file(path, std::ios::binary); // open file as binary file
+    if(!safetensors_file){
+        std::cerr << "could not open safetensors file";
+        return -1;
+    }
+    uint64_t headersize = 0;
+    // so read expects a char buffer to store the extracted data
+    // so we cast it to tell cpp to treat this variables memory as an 8-byte 
+    // destination buffer, we know its size is 8 bytes, but we can also use sizeof
+    safetensors_file.read(reinterpret_cast<char*>(&headersize),8);
+    if(!safetensors_file){
+        std::cerr << "could not read safetensors file";
+        return -1;
+    }
+    std::cout << headersize << '\n';
+    // this means the next headersize amount of bytes of the file contain the json header which
+    // describes all tensors. What we want to do now is allocate this amount of memory on the GPU 
+    // then create pointers to each of the tensors, using the offsets from shape value for each tensor
+    
+
+}
 int main()
 {
     checkGPUStatus();
+    loadModelHeader();
+
 
     return 0;
 }
